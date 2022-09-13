fd9a372742e5da47a781a931816fb4c3d42777eb
Victor <VictorCarmona9035@hotmail.com>
2022-09-13 19:31:33 +0200

    21-CAPA PRESENTACION:Eliminar registros

--- Sistema.Datos/DCategoria.cs
+++ Sistema.Datos/DCategoria.cs
@@ -1,11 +1,7 @@
 ﻿using Sistema.Entidades;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sistema.Datos
 {
